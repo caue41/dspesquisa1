2b9c7dd0ef7fb4ddc74735cf537173a5cdf73ac1
caue41 <69259714+caue41@users.noreply.github.com>
2020-09-19 18:08:08 -0300

    Adicionando a pagina de graficos versao final

--- front-web/src/pages/Charts/types.ts
+++ front-web/src/pages/Charts/types.ts
@@ -1,15 +1,7 @@
 import { Platform } from "../Records/types";
 
 export type Game = {
-<<<<<<< HEAD
-    id: number;
-    title: string;
-    platform: Platform;
-}
-
-=======
     id: number,
     title: string,
     platform: Platform;
 }
->>>>>>> 61dfad43bbed3f2a0f34725c739d88c9b40a08f9
